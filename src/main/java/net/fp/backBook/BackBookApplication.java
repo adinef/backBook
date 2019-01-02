fe5db60ff00002ac1ac3c72dd8eff94d93fac896
Adrian Fijałkowski <fijaladrian@gmail.com>
2019-01-02 21:36:33 +0100

    added Mongo config to properties + commented unused JPA dependency in pom.xml

--- src/main/java/net/fp/backBook/BackBookApplication.java
+++ src/main/java/net/fp/backBook/BackBookApplication.java
@@ -1,5 +1,7 @@
 package net.fp.backBook;
 
+import net.fp.backBook.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -7,6 +9,9 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 public class BackBookApplication {
+
+	@Autowired
+	UserRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackBookApplication.class, args);
