c76ecea4ee07123ab60b63bb794fef4d7ec895f2
Adrian Fijałkowski <fijaladrian@gmail.com>
2019-01-02 20:12:58 +0100

    Init project files

---
+++ src/main/java/net/fp/backBook/BackBookApplication.java
@@ -0,0 +1,14 @@
+package net.fp.backBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackBookApplication.class, args);
+	}
+
+}
+
