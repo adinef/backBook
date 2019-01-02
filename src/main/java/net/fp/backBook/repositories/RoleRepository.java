75bba4f29032513e43c4c6c2278e5be5be8fd860
Adrian Fijałkowski <fijaladrian@gmail.com>
2019-01-02 22:11:40 +0100

    Added repos

---
+++ src/main/java/net/fp/backBook/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package net.fp.backBook.repositories;
+
+import net.fp.backBook.model.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends MongoRepository<Role, String> {
+}
---
+++ src/main/java/net/fp/backBook/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package net.fp.backBook.repositories;
+
+import net.fp.backBook.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
