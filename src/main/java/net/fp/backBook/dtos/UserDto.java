550c8637ecefbd4eadda447a31e0d292ee2fac98
Adrian Fijałkowski <fijaladrian@gmail.com>
2019-01-03 16:48:19 +0100

    more methods in controller

--- src/main/java/net/fp/backBook/dtos/UserDto.java
+++ src/main/java/net/fp/backBook/dtos/UserDto.java
@@ -1,4 +1,25 @@
 package net.fp.backBook.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
+
+    private String id;
+
+    private String name;
+
+    private String lastName;
+
+    private String login;
+
+    private String password;
+
+    private String email;
 }
