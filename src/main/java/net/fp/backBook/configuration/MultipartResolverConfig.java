3a6531ccf9c4e3ba5d59ef7766e85ea1cc86180c
Paweł Pomarański <pawelpomaranski97@gmail.com>
2019-01-09 17:02:26 +0100

    Add MultipartResolverConfig

---
+++ src/main/java/net/fp/backBook/configuration/MultipartResolverConfig.java
@@ -0,0 +1,16 @@
+package net.fp.backBook.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class MultipartResolverConfig {
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(100000);
+        return multipartResolver;
+    }
+}
