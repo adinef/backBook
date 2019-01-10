1343a48d389afe1063d037c3263812acd6fa42d3
Adrian Fijałkowski <fijaladrian@gmail.com>
2019-01-10 23:06:16 +0100

    more UserControllerTests

--- src/test/java/net/fp/backBook/controllers/AuthenticationControllerTests.java
+++ src/test/java/net/fp/backBook/controllers/AuthenticationControllerTests.java
@@ -1,9 +1,7 @@
 package net.fp.backBook.controllers;
 
-import com.sun.deploy.net.HttpResponse;
 import net.fp.backBook.dtos.Credentials;
 import net.fp.backBook.dtos.TokenDto;
-import net.fp.backBook.exceptions.AuthenticationException;
 import net.fp.backBook.security.service.TokenService;
 import org.junit.*;
 import org.junit.runner.RunWith;
@@ -11,20 +9,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
--- src/test/java/net/fp/backBook/controllers/UserControllerTests.java
+++ src/test/java/net/fp/backBook/controllers/UserControllerTests.java
@@ -1,5 +1,6 @@
 package net.fp.backBook.controllers;
 
+import net.fp.backBook.dtos.Credentials;
 import net.fp.backBook.dtos.UserDto;
 import net.fp.backBook.exceptions.GetException;
 import net.fp.backBook.model.User;
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,16 +21,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ComponentScan(basePackages = {"net.fp.backBook.repositories",
+        "net.fp.backBook.services", "net.fp.backBook.controllers", "net.fp.backBook.configuration"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTests {
 
@@ -74,13 +79,12 @@ public class UserControllerTests {
 
     @Test
     public void testGetUsersBadRequestOnGetException() throws Exception {
-        List<User> users = Arrays.asList();
         when(userService.getAll()).thenThrow(GetException.class);
         try {
             mockMvc.perform(get("/users"))
                     .andExpect(status().isBadRequest())
                     .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                    .andExpect(jsonPath("$error").isNotEmpty());
+                    .andExpect(jsonPath("$.error").isNotEmpty());
             verify(userService).getAll();
         } catch (Exception e) {
 
@@ -97,6 +101,110 @@ public class UserControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(jsonPath("$.name").value("test"));
-        verify(userService).getById("xxx");
+        verify(userService).getById(anyString());
+    }
+
+    @Test
+    public void testGetUserByIdBadRequestOnGetException() throws Exception {
+        when(userService.getById(anyString())).thenThrow(GetException.class);
+        try {
+            String path = "/users/xxx";
+            mockMvc.perform(get(path))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                    .andExpect(jsonPath("$.error").isNotEmpty());
+            verify(userService).getById(anyString());
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void testGetByLoginReturns() throws Exception {
+        User user = User.builder().login("test").build();
+        when(userService.getUserByLogin(anyString())).thenReturn(user);
+        when(modelMapper.map(user, UserDto.class)).thenReturn(UserDto.builder().login("test").build());
+        String path = "/users/login/xxx";
+        mockMvc.perform(get(path))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.login").value("test"));
+        verify(userService).getUserByLogin(anyString());
+    }
+
+    @Test
+    public void testGetByLoginBadRequestOnGetException() throws Exception {
+        when(userService.getUserByLogin(anyString())).thenThrow(GetException.class);
+        try {
+            String path = "/users/login/xxx";
+            mockMvc.perform(get(path))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                    .andExpect(jsonPath("$.error").isNotEmpty());
+            verify(userService).getUserByLogin(anyString());
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void testGetByEmailReturns() throws Exception {
+        User user = User.builder().email("test").build();
+        when(userService.getUserByEmail(anyString())).thenReturn(user);
+        when(modelMapper.map(user, UserDto.class)).thenReturn(UserDto.builder().email("test").build());
+        String path = "/users/email/xxx";
+        mockMvc.perform(get(path))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.email").value("test"));
+        verify(userService).getUserByEmail(anyString());
+    }
+
+    @Test
+    public void testGetByEmailBadRequestOnGetException() throws Exception {
+        when(userService.getUserByEmail(anyString())).thenThrow(GetException.class);
+        try {
+            String path = "/users/email/xxx";
+            mockMvc.perform(get(path))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                    .andExpect(jsonPath("$.error").isNotEmpty());
+            verify(userService).getUserByEmail(anyString());
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void testGetByCredentialsReturns() throws Exception {
+        User user = User.builder().login("test").password("test").build();
+        when(userService.getUserByLoginAndPassword(anyString(), anyString())).thenReturn(user);
+        when(modelMapper.map(user, UserDto.class)).thenReturn(UserDto.builder().login("test").password("test").build());
+        String path = "/users/credentials";
+        mockMvc.perform(post(path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"login\": \"test\", \"password\": \"test\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.login").value("test"))
+                .andExpect(jsonPath("$.password").value("test"));
+        verify(userService).getUserByLoginAndPassword(anyString(), anyString());
+    }
+
+    @Test
+    public void testGetByCredentialsBadRequestOnGetException() throws Exception {
+        when(userService.getUserByLoginAndPassword(anyString(), anyString())).thenThrow(GetException.class);
+        try {
+            String path = "/users/credentials";
+            mockMvc.perform(post(path)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"login\": \"test\", \"password\": \"test\"}"))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                    .andExpect(jsonPath("$.error").isNotEmpty());
+            verify(userService).getUserByLoginAndPassword(anyString(), anyString());
+        } catch (Exception e) {
+
+        }
     }
 }
