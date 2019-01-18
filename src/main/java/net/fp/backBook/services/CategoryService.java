dee62ebdb6952c22fa1e97ff8dd4c2fc1b8d4dc8
Paweł Pomarański <pawelpomaranski97@gmail.com>
2019-01-18 12:35:28 +0100

    Add CategoryService

---
+++ src/main/java/net/fp/backBook/services/CategoryService.java
@@ -0,0 +1,21 @@
+package net.fp.backBook.services;
+
+import net.fp.backBook.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CategoryService {
+    Category getById(String id);
+
+    List<Category> getAll();
+
+    void delete(String id);
+
+    Category add(Category category);
+
+    Category modify(Category category);
+
+    Category getByName(String name);
+}
